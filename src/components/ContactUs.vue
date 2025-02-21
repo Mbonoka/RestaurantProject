3bd6be7a84ad0455f552318e3731e3617df9640d
Mbonoka <esther.mbonoka@strathmore.edu>
2025-02-21 12:15:14 +0300

    Menu&About

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -4,3 +4,5 @@
 <script setup>
 
 </script>
+
+
--- src/components/MenuList.vue
+++ src/components/MenuList.vue
@@ -1,117 +1,153 @@
 <template>
     <v-container>
-        <v-container>
+      <v-container>
         <v-card color="blue" >
             <v-card-title>
                 <div class="text-h3">Our Menu</div>
             </v-card-title>
         </v-card>
-    </v-container>
-            <v-card class="mx-auto" elevation="24">
-              <v-container>
+      </v-container>
+        <v-card class="mx-auto" elevation="24">
+            <v-container>
+            <v-row>
+                <v-col cols="12" md="4" v-for="item in menuItems":key=item>
+                    <v-card>
+                        <v-card  color="#D79828">
+                            <v-card-item>
+                                <v-card-title>{{ item.name }}</v-card-title>
 
-                 <v-row>
-                    <v-col cols="12" md="4" v-for="item in menuItems" :key=item>
-                       <v-card>
-                            <v-card color="#D79828">
-                                <v-card-item>
-                                    <v-card-title>Chocolate Milkshake</v-card-title>
-                                        <v-card-subtitle> velvety chocolate ice cream with creamy milk </v-card-subtitle>
-                                </v-card-item>
-                                <v-img height="300px" src="/shake3.jpg" cover ></v-img>
-                                <v-card-text> 450 </v-card-text>
-                            </v-card><br><br>
-
-                          <v-card color="#C18A3E">
-                                <v-card-item>
-                                <v-card-title>Chips</v-card-title>
-    
-                                <v-card-subtitle>Best chips in town</v-card-subtitle>
-                                </v-card-item>
-                                <v-img height="300px" src="https://unsplash.com/photos/a-basket-of-french-fries-sitting-on-top-of-a-wooden-table-ChXHveqrb28?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash" cover ></v-img>
-    
-                                <v-card-text> 200 </v-card-text>
-                            </v-card><br><br>
-                            <v-card color="brown">
-                                <v-card-item>
-                                    <v-card-title>Sausage</v-card-title>
-                                        <v-card-subtitle>Best sausages in town </v-card-subtitle>
-                                </v-card-item>
-                                <v-img height="300px" src="https://unsplash.com/photos/sausage-on-black-round-pan-cSxpCQrRlo8" cover ></v-img>
-                                <v-card-text> 450 </v-card-text>
-                            </v-card><br><br>
-                            <v-card color="#D79828">
-                                <v-card-item>
-                                    <v-card-title>Chocolate Milkshake</v-card-title>
-                                        <v-card-subtitle> velvety chocolate ice cream with creamy milk </v-card-subtitle>
-                                </v-card-item>
-                                <v-img height="300px" src="/shake3.jpg" cover ></v-img>
-                                <v-card-text> 450 </v-card-text>
-                            </v-card><br><br>
-                            <v-card color="#D79828">
-                                <v-card-item>
-                                    <v-card-title>Chocolate Milkshake</v-card-title>
-                                        <v-card-subtitle> velvety chocolate ice cream with creamy milk </v-card-subtitle>
-                                </v-card-item>
-                                <v-img height="300px" src="/shake3.jpg" cover ></v-img>
-                                <v-card-text> 450 </v-card-text>
-                            </v-card><br><br>
-                            <v-card color="#D79828">
-                                <v-card-item>
-                                    <v-card-title>Chocolate Milkshake</v-card-title>
-                                        <v-card-subtitle> velvety chocolate ice cream with creamy milk </v-card-subtitle>
-                                </v-card-item>
-                                <v-img height="300px" src="/shake3.jpg" cover ></v-img>
-                                <v-card-text> 450 </v-card-text>
-                            </v-card><br><br>
-                            <v-card color="#D79828">
-                                <v-card-item>
-                                    <v-card-title>Chocolate Milkshake</v-card-title>
-                                        <v-card-subtitle> velvety chocolate ice cream with creamy milk </v-card-subtitle>
-                                </v-card-item>
-                                <v-img height="300px" src="/shake3.jpg" cover ></v-img>
-                                <v-card-text> 450 </v-card-text>
-                            </v-card><br><br>
-
-
-
-
-
+                                <v-card-subtitle>{{ item.description }} </v-card-subtitle>
+                            </v-card-item>
+                            <v-img height="200px":src="item.image"cover></v-img>
+                        <v-card-text>
+                        {{ item.price }}
+                        </v-card-text>
+                        <v-btn color="black" variant="tonial" @click="addToOrder(item)"> Order</v-btn>
                         </v-card>
-                     </v-col>
-                     </v-row>
-                </v-container>
-            </v-card>
+                        
+                    </v-card>
+
+                </v-col>
+            </v-row>
+            </v-container>
+        </v-card>
+    </v-container>
+    <v-container>
+        <v-card color="blue" >
+            <v-card-title>
+                <div class="text-h6">Current order: {{totalOrderAmount}}</div>
+            </v-card-title>
+        </v-card>
+    </v-container>
+    <v-container>
+        <v-card class="mx-auto" elevation="24">
+            <v-container>
+                <v-row>
+                    <v-col cols="12" md="4" v-for="item in orderItems" :key=item>
+                        <v-card color="yellow">
+                            <v-card-item>
+                            <v-card-title>{{ item.name }}</v-card-title>
+
+                            <v-card-subtitle>{{ item.description }}</v-card-subtitle>
+                            </v-card-item>
+                            <v-img height="200px" :src="item.image" cover ></v-img>
+
+                            <v-card-text>{{ item.price }}</v-card-text>
+                        </v-card>
+                        </v-col>
+                </v-row>
+            </v-container>
+        </v-card>
     </v-container>
 </template>
+
 <script setup>
+import OrderPage from './OrderPage.vue'
 const menuItems = [
     {
-        name: "Chips",
-        price: 200,
+        name: "French Fries",
+        price: Ksh250,
         description: "Best chips in town",
         size: "Large",
         offers: "None",
-        image: "https://unsplash.com/photos/a-basket-of-french-fries-sitting-on-top-of-a-wooden-table-ChXHveqrb28?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash"
+        image: "/chips2.jpg"
     },
     {
         name: "Sausage",
-        price: 50,
+        price: 100,
         description: "Best sausages in town",
         size: "None",
         offers: "None",
-        image: "https://unsplash.com/photos/sausage-on-black-round-pan-cSxpCQrRlo8"
+        image: "/sausages1.jpg"
     },
 
     {
         name: "Avocado and Egg Toast",
         price: 400,
-        description: "Best combo in town",
+        description: "Scrambling in depth",
         size: "None",
         offers: "None",
         image: "/food6.jpg"
+    
     },
     {
+        name: "Pancake Stack",
+        price: 250,
+        description: "Flavourful taste",
+        size: "Large",
+        offers: "None",
+        image: "/pancake1.jpg"
+    },
+    {
+        name: "Vanilla cake",
+        price: 450,
+        description: "Best in the game",
+        size: "None",
+        offers: "None",
+        image: "/cake1.jpg"
+    },
+
+    {
+        name: "pistachio and raspberries cake",
+        price: 400,
+        description: "Unleash into flavour",
+        size: "None",
+        offers: "None",
+        image: "/cake2.jpg"
+    },
+    {
+        name: "Grilled beef burger",
+        price: 500,
+        description: "Unbeatable taste",
+        size: "None",
+        offers: "None",
+        image: "/burger1.jpg"
+    },
+    {
+        name: "Vegan Burger",
+        price: 500,
+        description: "New",
+        size: "None",
+        offers: "None",
+        image: "/burger3.jpg"
+    },
+     {
+        name: "Eggs Benedict Burger",
+        price: 500,
+        description: "Tasteful",
+        size: "None",
+        offers: "None",
+        image: "/Burger2.jpg"
+    },
+    {
+        name: "Chicken burger",
+        price: 500,
+        description: "Unbeatable",
+        size: "None",
+        offers: "None",
+        image: "/food4.jpg"
+    },
+        {
         name: "Salmon and Zucchini",
         price: 2500,
         description: "Best combo in town",
@@ -120,39 +156,42 @@ const menuItems = [
         image: "/food2.jpg"
     },
     {
-        name: "Pancake Stack",
-        price: 200,
-        description: "Best chips in town",
-        size: "Large",
-        offers: "None",
-        image: "/pancake.jpg"
-    },
-    {
-        name: "Vanilla cake",
-        price: 450,
-        description: "Unleash into our vanilla cake",
+        name: "Sinful sip   ",
+        price: 550,
+        description: "Top-Notch",
         size: "None",
         offers: "None",
-        image: ""
+        image: "/wine1.jpg"
     },
-
     {
-        name: "pistachio and raspberries cake",
+        name: "Rutini   ",
         price: 400,
-        description: "Best combo in town",
+        description: "unhealthy heavenly feeling",
         size: "None",
         offers: "None",
-        image: "/food6.jpg"
+        image: "/wine5.jpg"
     },
     {
-        name: "Salmon and Zucchini",
-        price: 1500,
-        description: "Best combo in town",
+        name: "D Vajra Langhe   ",
+        price: 400,
+        description: "From the Langhe hills",
         size: "None",
         offers: "None",
-        image: "/food2.jpg"
+        image: "/wine2.jpg"
     }
-    
+
     
 ]
+//current state of order
+var orderItem = []
+var totalOrderAmount = 0
+function calculateTotalAmount(price){
+    totalOrderAmount += price
+    }
+function addToOrder(item){
+    orderItems.push(item)
+    calculateTotalAmount(item.price)
+}
+
+
 </script>
